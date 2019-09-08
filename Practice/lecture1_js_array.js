574688cbf66aac5b0465a6020346a5cee7b9622a
subbak2 <subbak2@likelion.org>
2019-09-08 16:41:33 +0900

    190908

---
+++ Practice/lecture1_js_array.js
@@ -0,0 +1,4 @@
+var newArr = ["apple", "tomato"].map(function (value, index) {
+    return index + "번째 과일은 " + value + "입니다";
+});
+console.log(newArr)// 여러분들이 실행해보세요
---
+++ Practice/lecture2_json.js
@@ -0,0 +1,4 @@
+var obj = { name: "crong", age: 20 }
+
+console.log(obj["name"]);
+console.log(obj.age);
